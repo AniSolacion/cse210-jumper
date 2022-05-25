ba9149e6367e19b825ad2b619237ab24efc60bf0
Logan Huston <shadows7590@gmail.com>
2022-05-25 12:00:10 -0600

    Added comments to parachute class.

--- Game/Parachute.cs
+++ Game/Parachute.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 namespace Unit03.Game
 {
+    // The parachute class keeps track of a parachute and it's cargo (person in this instance).
+    // The parachachute can be destroyed one line at a time, and the cargo(person) can die.
     public class Parachute
     {
         private List<string> parachute = new List<string>();
         private List<string> person = new List<string>();
 
+        // IConstructs a new instance of parachute.
+        // Adds the lines of the parachute and adds the lines of the person.
         public Parachute()
         {
             parachute.Add("   ___   ");
@@ -20,21 +24,26 @@ namespace Unit03.Game
             person.Add("   / \\   ");
         }
 
+        // Get rid of the first row of the parachute.
         public void takeRowOffParachute()
         {
             parachute.RemoveAt(0);
         }
+        
+        // When the parachute is gone, set the person to x rather than an o.
+        public void setDeath()
+        {
+            person[0] = "    x    ";
+        }
+
         public List<string> getParachute()
         {
             return parachute;
         }
+
         public List<string> getPerson()
         {
             return person;
-        }
-        public void setDeath()
-        {
-            person[0] = "    x    ";
         }
     }
 }
